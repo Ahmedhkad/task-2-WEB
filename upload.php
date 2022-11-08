5c0e5a74fb6e83c35ed05a651854860115c8c70a
ahkad-laptop <ahkad@laptop.com>
2022-11-08 15:42:43 +0300

    show data inside file csv in html table

--- upload.php
+++ upload.php
@@ -1,26 +1,42 @@
-<?php
-// Создать PHP-страницу upload.php с формой загрузки CSV-файла
-// В CSV-файле должны быть 2 столбца: название файла, содержимое
-// Рядом с файлом upload.php требуется создать папку /upload/ и создать в ней файлы, прочитав CSV-файл.
-// Какие дыры это может создать? Как бороться?
-// Ограничений на функции и возможности PHP нет.
-
-// Пример файла CSV:
-// aaa.txt,Привет
-// bbb.log,Тест
-// ccc.html,Заголовок
-
-// При загрузке такого файла должны быть созданы /upload/1.txt, /upload/2.log, /upload/3.html (с соответствующим содержимым)
- 
-$row = 1;
-if (($handle = fopen("Example.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num fields in line $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
+<html>
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
     }
-    fclose($handle);
-}
+</style>
+
+<body>
+
+    <?php
+    // Создать PHP-страницу upload.php с формой загрузки CSV-файла
+    // В CSV-файле должны быть 2 столбца: название файла, содержимое
+    // Рядом с файлом upload.php требуется создать папку /upload/ и создать в ней файлы, прочитав CSV-файл.
+    // Какие дыры это может создать? Как бороться?
+    // Ограничений на функции и возможности PHP нет.
+
+    // Пример файла CSV:
+    // aaa.txt,Привет
+    // bbb.log,Тест
+    // ccc.html,Заголовок
+
+    // При загрузке такого файла должны быть созданы /upload/1.txt, /upload/2.log, /upload/3.html (с соответствующим содержимым)
+
+    if (($handle = fopen("./upload/Example.csv", "r")) !== FALSE) { //load file .csv
+        echo "<table>\n  <tr>           
+        <th>File Name</th>\n
+        <th>Containt</th>\n
+      </tr>";
+        while (($data = fgetcsv($handle, 1000, "r")) !== FALSE) {   //Get data inside
+            foreach ($data as $key => $value) {        //extract value from data array
+                $pieces = explode(";", $value);        // split table row by ";" 
+                echo "<tr> \n  <td>";
+                echo  $pieces[0];       // colume 1 - file name
+                echo "</td> \n <td>";
+                echo $pieces[1];        // colume 2 - file containt
+                echo "</td> \n";
+            }
+        }
+        fclose($handle);
+    }
